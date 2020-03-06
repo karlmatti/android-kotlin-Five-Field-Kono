c66e5005901a5b57f249b108b7a268f5b8827c6e
karlmatti <karlmatti@hotmail.com>
2020-03-06 16:11:45 +0200

    trying to add more business logic, added handlePlay, updated game ui

--- app/src/main/java/karlmatti/application/hw1/MainActivity.kt
+++ app/src/main/java/karlmatti/application/hw1/MainActivity.kt
@@ -6,30 +6,87 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.game_board.*
+import kotlinx.android.synthetic.main.game_statistics.*
 
 class MainActivity : AppCompatActivity() {
     private var counter = 0
     private lateinit var lastClickButton: Button
-    private lateinit var gameBoard: List<List<Button>>
-    private lateinit var player1Color: String
-    private lateinit var player2Color: String
+    private lateinit var gameBoardButtons: List<List<Button>>
+    private var gameBoardMatrix = Array(5) { Array(5) {0}}
+    private var player1Score = 0
+    private var player2Score = 0
+    private val player1Color = R.drawable.player1_button
+    private val player2Color = R.drawable.player2_button
     private lateinit var whoseTurn: Enum<Player>
+    private var gameStarted: Boolean = false
+    private lateinit var gameState: MutableMap<String, Any>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        whoseTurn = Player.One
+        Log.d("OnCreate", "x")
+    }
+    fun handlePlay(btn: View) {
+        //TODO: Reset game board in gameBoardMatrix and gameBoardButtons
+        //TODO: Set whoseTurn depending if the switch is turned off or on
+        //TODO: Save the state
+
+        resetGameBoard()
+        saveSate()
+
+
+    }
+    private fun saveSate() {
         val row0 = listOf(button00, button01, button02, button03, button04)
         val row1 = listOf(button10, button11, button12, button13, button14)
         val row2 = listOf(button20, button21, button22, button23, button24)
         val row3 = listOf(button30, button31, button32, button33, button34)
         val row4 = listOf(button40, button41, button42, button43, button44)
-        gameBoard= listOf(row0, row1, row2, row3, row4)
-        player1Color = button40.background.constantState.toString()
-        player2Color = button00.background.constantState.toString()
-        whoseTurn = Player.One
-    }
 
+        gameState["whoseTurn"] = whoseTurn
+        gameState["Mode"] = game_mode.selectedItem.toString()
+        gameState["Started"] = true
+        gameState["Board"] = listOf(row0, row1, row2, row3, row4)
+
+
+    }
+    private fun resetGameBoard(){
+        //TODO: Reset also gameBoardMatrix
+        button00.setBackgroundResource(R.drawable.player2_button)
+        button01.setBackgroundResource(R.drawable.player2_button)
+        button02.setBackgroundResource(R.drawable.player2_button)
+        button03.setBackgroundResource(R.drawable.player2_button)
+        button04.setBackgroundResource(R.drawable.player2_button)
+
+        button10.setBackgroundResource(R.drawable.player2_button)
+        button11.setBackgroundResource(R.drawable.empty_button)
+        button12.setBackgroundResource(R.drawable.empty_button)
+        button13.setBackgroundResource(R.drawable.empty_button)
+        button14.setBackgroundResource(R.drawable.player2_button)
+
+        button20.setBackgroundResource(R.drawable.empty_button)
+        button21.setBackgroundResource(R.drawable.empty_button)
+        button22.setBackgroundResource(R.drawable.empty_button)
+        button23.setBackgroundResource(R.drawable.empty_button)
+        button24.setBackgroundResource(R.drawable.empty_button)
+
+        button30.setBackgroundResource(R.drawable.player1_button)
+        button31.setBackgroundResource(R.drawable.empty_button)
+        button32.setBackgroundResource(R.drawable.empty_button)
+        button33.setBackgroundResource(R.drawable.empty_button)
+        button34.setBackgroundResource(R.drawable.player1_button)
+
+        button40.setBackgroundResource(R.drawable.player1_button)
+        button41.setBackgroundResource(R.drawable.player1_button)
+        button42.setBackgroundResource(R.drawable.player1_button)
+        button43.setBackgroundResource(R.drawable.player1_button)
+        button44.setBackgroundResource(R.drawable.player1_button)
+
+    }
     fun handleClick(btn: View) {
+        //TODO: Handle also gameBoardMatrix -> create new method for making move makeMove(from, to) etc.
         val button = btn as Button
         counter++
         if (counter % 2 == 0) {
@@ -42,8 +99,10 @@ class MainActivity : AppCompatActivity() {
                 Log.d("whoWon()", whoWon().toString())
                 changeTurn()
             }
+            lastClickButton.text = ""
         } else {
             lastClickButton = button
+            button.text = "x"
         }
 
 
@@ -58,7 +117,7 @@ class MainActivity : AppCompatActivity() {
         var col: Int = 0
         for (i in 0..4) {
             for (j in 0..4) {
-                if (gameBoard[i][j] == firstlyClickedBtn){
+                if (gameBoardButtons[i][j] == firstlyClickedBtn){
                     row = i
                     col = j
                     Log.d("isMovedDiagonally_row", row.toString())
@@ -71,47 +130,48 @@ class MainActivity : AppCompatActivity() {
         //north
         if(row == 0){ // When movable button is in first row
             if(col == 0) {
-                if(gameBoard[row+1][col+1] == secondlyClickedBtn) return true
+                if(gameBoardButtons[row+1][col+1] == secondlyClickedBtn) return true
             } else if(col == 4) {
-                if(gameBoard[row+1][col-1] == secondlyClickedBtn) return true
+                if(gameBoardButtons[row+1][col-1] == secondlyClickedBtn) return true
             } else {
-                if(gameBoard[row+1][col+1] == secondlyClickedBtn) return true
-                else if(gameBoard[row+1][col-1] == secondlyClickedBtn) return true
+                if(gameBoardButtons[row+1][col+1] == secondlyClickedBtn) return true
+                else if(gameBoardButtons[row+1][col-1] == secondlyClickedBtn) return true
             }
         }
         //south
         else if(row == 4){ // When movable button is in last row
             if(col == 0) {
-                if(gameBoard[row-1][col+1] == secondlyClickedBtn) return true
+                if(gameBoardButtons[row-1][col+1] == secondlyClickedBtn) return true
             } else if(col == 4) {
-                if(gameBoard[row-1][col-1] == secondlyClickedBtn) return true
+                if(gameBoardButtons[row-1][col-1] == secondlyClickedBtn) return true
             } else {
-                if(gameBoard[row-1][col+1] == secondlyClickedBtn) return true
-                else if(gameBoard[row-1][col-1] == secondlyClickedBtn) return true
+                if(gameBoardButtons[row-1][col+1] == secondlyClickedBtn) return true
+                else if(gameBoardButtons[row-1][col-1] == secondlyClickedBtn) return true
             }
         }
         //east
         else if(col == 4){ // When movable button is in last column
-            if(gameBoard[row+1][col-1] == secondlyClickedBtn) return true
-            else if(gameBoard[row-1][col-1] == secondlyClickedBtn) return true
+            if(gameBoardButtons[row+1][col-1] == secondlyClickedBtn) return true
+            else if(gameBoardButtons[row-1][col-1] == secondlyClickedBtn) return true
         }
 
         //west
         else if(col == 0){ // When movable button is in first column
-            if(gameBoard[row+1][col+1] == secondlyClickedBtn) return true
-            else if(gameBoard[row-1][col+1] == secondlyClickedBtn) return true
+            if(gameBoardButtons[row+1][col+1] == secondlyClickedBtn) return true
+            else if(gameBoardButtons[row-1][col+1] == secondlyClickedBtn) return true
         }
         //rest
-        else if(gameBoard[row+1][col+1] == secondlyClickedBtn) return true
-        else if(gameBoard[row+1][col-1] == secondlyClickedBtn) return true
-        else if(gameBoard[row-1][col+1] == secondlyClickedBtn) return true
-        else if(gameBoard[row-1][col-1] == secondlyClickedBtn) return true
+        else if(gameBoardButtons[row+1][col+1] == secondlyClickedBtn) return true
+        else if(gameBoardButtons[row+1][col-1] == secondlyClickedBtn) return true
+        else if(gameBoardButtons[row-1][col+1] == secondlyClickedBtn) return true
+        else if(gameBoardButtons[row-1][col-1] == secondlyClickedBtn) return true
 
         return false
 
     }
 
     private fun isMovingSelfButtons(button: Button): Boolean {
+        //TODO: Should compare values in business logic not UI id values
         if(button.background.constantState.toString() == player1Color &&
             whoseTurn == Player.One){
             return true
@@ -123,24 +183,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun whoWon(): Player {
+        //TODO: Should compare values in business logic not UI id values
         //Check if Player1 has won
-        if (gameBoard[0][0].background.constantState.toString() == player1Color &&
-            gameBoard[0][1].background.constantState.toString() == player1Color &&
-            gameBoard[0][2].background.constantState.toString() == player1Color &&
-            gameBoard[0][3].background.constantState.toString() == player1Color &&
-            gameBoard[0][4].background.constantState.toString() == player1Color &&
-            gameBoard[1][0].background.constantState.toString() == player1Color &&
-            gameBoard[1][4].background.constantState.toString() == player1Color
+        if (gameBoardButtons[0][0].background.constantState.toString() == player1Color &&
+            gameBoardButtons[0][1].background.constantState.toString() == player1Color &&
+            gameBoardButtons[0][2].background.constantState.toString() == player1Color &&
+            gameBoardButtons[0][3].background.constantState.toString() == player1Color &&
+            gameBoardButtons[0][4].background.constantState.toString() == player1Color &&
+            gameBoardButtons[1][0].background.constantState.toString() == player1Color &&
+            gameBoardButtons[1][4].background.constantState.toString() == player1Color
                 ) return Player.One
 
         //Check if Player2 has won
-        if (gameBoard[4][0].background.constantState.toString() == player2Color &&
-            gameBoard[4][1].background.constantState.toString() == player2Color &&
-            gameBoard[4][2].background.constantState.toString() == player2Color &&
-            gameBoard[4][3].background.constantState.toString() == player2Color &&
-            gameBoard[4][4].background.constantState.toString() == player2Color &&
-            gameBoard[3][0].background.constantState.toString() == player2Color &&
-            gameBoard[3][4].background.constantState.toString() == player2Color
+        if (gameBoardButtons[4][0].background.constantState.toString() == player2Color &&
+            gameBoardButtons[4][1].background.constantState.toString() == player2Color &&
+            gameBoardButtons[4][2].background.constantState.toString() == player2Color &&
+            gameBoardButtons[4][3].background.constantState.toString() == player2Color &&
+            gameBoardButtons[4][4].background.constantState.toString() == player2Color &&
+            gameBoardButtons[3][0].background.constantState.toString() == player2Color &&
+            gameBoardButtons[3][4].background.constantState.toString() == player2Color
         ) return Player.Two
 
         return Player.None
